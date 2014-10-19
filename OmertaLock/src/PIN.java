ee61e41ff3c3f870b6b3bd63d4c08c5957d8cd88
gbrothers93@gmail.com <gbrothers93@gmail.com@c614bec4-09f8-51cd-9207-0c3074abb597>
2014-10-19 05:56:20 +0000

    Created the skeleton for the PIN class.

---
+++ OmertaLock/src/PIN.java
@@ -0,0 +1,58 @@
+
+public class PIN
+{
+	private int pinLength;
+	private int pin;
+	private enum pinType {ADMIN, addPIN, deletePIN, toggleBeeper, relockTime};
+	private boolean pinEnabled = true;
+	
+	
+	/**
+	 * Set the pin length.
+	 */
+	public void setPinLength()
+	{
+		
+	}
+	
+	/**
+	 * Set the pin.
+	 */
+	public void setPIN()
+	{
+		
+	}
+	
+	/**
+	 * Enable PIN.
+	 */
+	public void enablePIN()
+	{
+		this.pinEnabled = true;
+	}
+	
+	/**
+	 * Disable PIN.
+	 */
+	public void disablePIN()
+	{
+		this.pinEnabled = false;
+	}
+	
+	/**
+	 * Get the PIN type.
+	 */
+	public void getPinType()
+	{
+		
+	}
+	
+	/**
+	 * Check if the PIN is equal.
+	 */
+	public void equals()
+	{
+		
+	}
+	
+}
