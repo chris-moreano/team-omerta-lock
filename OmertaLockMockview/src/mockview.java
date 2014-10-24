ec03a141a096e5a9dc6768d8fcaeb89d6f4ec525
xtian071989@gmail.com <xtian071989@gmail.com@c614bec4-09f8-51cd-9207-0c3074abb597>
2014-10-24 17:16:39 +0000

    Experimenting with GUI Clicking Zero results in sound + message

--- OmertaLockMockview/src/mockview.java
+++ OmertaLockMockview/src/mockview.java
@@ -1,8 +1,10 @@
-import java.awt.GridBagConstraints;
-import java.awt.Insets;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
 
@@ -12,23 +14,52 @@ public class mockview extends JFrame {
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-	JButton a[] = new JButton[13]; // Array of Buttons
+	public static final int KEY_SIZE = 12;
+	JButton a[] = new JButton[KEY_SIZE]; // Array of Buttons
+	
 	public mockview(){
-		super("Omerta Electronix Mockview");
-		setSize(600,400); // Window Size
+		super("Keyboard");
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 
 		JPanel p = new JPanel();
+		p.setLayout(new GridLayout(4,3)); //4 Rows/3 Columns
 		
-		GridBagConstraints gbc = new GridBagConstraints();
-		gbc.insets = new Insets(15,15,15,15);
-		for(int i = 0 ; i < 13 ; i++)
+		for(int i = 0 ; i < KEY_SIZE ; i++)
 		{
-			a[i] = new JButton("" + i); 
-			gbc.gridy = i;
-			p.add(a[i]);	// add to JPanel p
-		}
+		
+			if(i == 10) // Tenth item is the asterisk
+			{
+				a[i] = new JButton("*");
+				p.add(a[i]);	// add to JPanel p
 
-		add(p); // add JPanel p to JFrame
+			}
+			else if(i == 11) // Eleventh item is the aterik
+			{
+				a[i] = new JButton("#");
+				p.add(a[i]);	// add to JPanel p
+
+			}
+			 
+			 else
+			 {
+				 a[i] = new JButton("" + i); 
+				 p.add(a[i]);	// add to JPanel p
+			 }
+
+		}
+		
+		add(p);	 	// add JPanel p to JFrame
+		pack(); 	// tight components
+		
+a[0].addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+				java.awt.Toolkit.getDefaultToolkit().beep();
+				JOptionPane.showMessageDialog(null, "Thanks 4 Clic");
+
+			}
+		});
 	}
 }
