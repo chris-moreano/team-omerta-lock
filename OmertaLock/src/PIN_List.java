effd9e9ef4f1c8bf31348e88ea09b441002d5dab
gbrothers93@gmail.com <gbrothers93@gmail.com@c614bec4-09f8-51cd-9207-0c3074abb597>
2014-11-06 21:53:51 +0000

    PIN_List method skeleton.

---
+++ OmertaLock/src/PIN_List.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class PIN_List extends PIN
+{
+
+	Hashtable pinList;
+	
+	
+	public void addPIN()
+	{
+		
+	}
+	
+	public void checkPinType()
+	{
+		getPinType();
+	}
+	
+	public void removePIN()
+	{
+		
+	}
+	
+	public void hashFunction()
+	{
+		
+	}
+	
+}
