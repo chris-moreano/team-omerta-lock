3d97290681dbcf6fa211e4fff1bfb9b56e8699d0
gbrothers93@gmail.com <gbrothers93@gmail.com@c614bec4-09f8-51cd-9207-0c3074abb597>
2014-11-21 21:33:58 +0000

    Implemented the hashtable in PIN_List

--- OmertaLock/src/PIN_List.java
+++ OmertaLock/src/PIN_List.java
@@ -2,13 +2,19 @@ import java.util.*;
 
 public class PIN_List extends PIN
 {
+	Hashtable<String, String> pinList;
 
-	Hashtable pinList = new Hashtable();
 	
-	
-	public void addPIN()
+	public PIN_List()
 	{
-		
+
+		pinList = new Hashtable<String, String>();
+	}
+	
+	
+	public void addPIN(String key, String value)
+	{
+		pinList.put(key, value);
 	}
 	
 	public void checkPinType()
@@ -16,9 +22,9 @@ public class PIN_List extends PIN
 		getPinType();
 	}
 	
-	public void removePIN()
+	public void removePIN(String key)
 	{
-		
+		pinList.remove(key);
 	}
 	
 	public void hashFunction()
