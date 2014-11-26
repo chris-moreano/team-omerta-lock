e662efdf8a3dc4658f7633b8e06f34c69473467e
cuadicles@gmail.com <cuadicles@gmail.com@c614bec4-09f8-51cd-9207-0c3074abb597>
2014-11-26 07:46:22 +0000

    Basic hash table.

--- OmertaLock/src/PIN_List.java
+++ OmertaLock/src/PIN_List.java
@@ -2,26 +2,72 @@ import java.util.*;
 
 public class PIN_List extends PIN
 {
-	Hashtable<String, String> pinList;
+	private static final int DEFAULT_TABLE_SIZE = 100; 
+	final int DEFAULT_TABLE_VALUE = 0; 
+	int[] hashTable;
+	int currentSize; 
 
 	/**
 	 * Constructor
 	 */
 	public PIN_List()
 	{
-
-		pinList = new Hashtable<String, String>();
+		this(DEFAULT_TABLE_SIZE); 
+	}
+	
+	 
+	private PIN_List( int size )
+	{
+		allocateHashTable(size); 
+		makeEmpty(); 
+	}
+	
+	private void allocateHashTable( int size )
+	{
+		hashTable = new int[size]; 
+	}
+	
+	private void makeEmpty()
+	{
+		currentSize = 0; 
+		for( int i = 0; i < hashTable.length; i++ )
+			hashTable[ i ] = DEFAULT_TABLE_VALUE; 
 	}
 	
 	/**
-	 * Adds a pin to the hashtable.
-	 * @param pinType Type of pin
-	 * @param pin Pin
+	 * 
+	 * @param pin
 	 */
-	public void addPIN(String pinType, String pin)
+	public void insert( int pin )
 	{
-		pinList.put(pinType, pin);
+		// 1. Make sure the PIN isn't already 
+		//    in the hash table. 
+		// 2. Hash the PIN to determine what 
+		//    index to insert it at. 
+		// 3. Assign the PIN to the index. 
+		
+		// 1
+		if ( !contains(pin) )
+			// 2 and 3
+			hashTable[ hash(pin) ] = pin;		
 	}
+	
+	public void remove( int pin )
+	{
+	
+	}
+	
+	public boolean contains( int pin )
+	{
+		return true; 
+	}
+	
+	public int hash( int pin )
+	{
+		return (pin % 99); 
+	}
+	
+	
 	
 	/*
 	 * Check the pinType
@@ -30,19 +76,4 @@ public class PIN_List extends PIN
 	{
 		getPinType();
 	}
-	
-	/**
-	 * Remove pin
-	 * @param pinType The pin type to be remove.
-	 */
-	public void removePIN(String pinType)
-	{
-		pinList.remove(pinType);
-	}
-	
-	public void hashFunction()
-	{
-		
-	}
-	
 }
