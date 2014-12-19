a9fd8ca2a5cf5d2f583ce700d7f49baf6bd0ba28
gbrothers93@gmail.com <gbrothers93@gmail.com@c614bec4-09f8-51cd-9207-0c3074abb597>
2014-12-19 07:54:57 +0000

    Added comments to InputEvent.java

--- OmertaLockHome/src/InputEvent.java
+++ OmertaLockHome/src/InputEvent.java
@@ -1,9 +1,18 @@
 import java.util.EventObject;
 
+/**
+ * Input events for the buttons
+ *
+ */
 public class InputEvent extends EventObject 
 {
 	private String btnID; 
 	
+	/**
+	 * Event handler
+	 * @param source the object event
+	 * @param btnID the button id
+	 */
 	public InputEvent(Object source, String btnID)
 	{
 		super(source);
@@ -11,6 +20,10 @@ public class InputEvent extends EventObject
 		this.btnID = btnID; 
 	}
 	
+	/**
+	 * Get the id for the button.
+	 * @return the button id
+	 */
 	public String getBtnID()
 	{
 		return btnID; 
