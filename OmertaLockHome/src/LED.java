520fe2fe755351e20aa2abaeaa0fd0bd5d45ac83
gbrothers93@gmail.com <gbrothers93@gmail.com@c614bec4-09f8-51cd-9207-0c3074abb597>
2014-12-19 08:41:09 +0000

    Added comments to LED.java

--- OmertaLockHome/src/LED.java
+++ OmertaLockHome/src/LED.java
@@ -1,6 +1,10 @@
 import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 
+/**
+ * Class that simulates LED lights.
+ *
+ */
 public class LED extends JLabel
 {
 	// "Global" members
@@ -22,26 +26,48 @@ public class LED extends JLabel
 	private int numberOfBlinks;
 	private ImageIcon restingColor; 
 	
+	/**
+	 * Constructor
+	 */
 	public LED()
 	{
 		this(GREEN_OFF, GREEN_ON, ONE_BLINK);
 	}
 	
+	/**
+	 * Constructor
+	 * @param restingColor the off color
+	 */
 	public LED (String restingColor)
 	{
 		 this(restingColor, GREEN_ON, ONE_BLINK);  
 	}
 	
+	/**
+	 * Constructor
+	 * @param numberOfBlinks the number of blinks
+	 */
 	public LED (int numberOfBlinks)
 	{
 		this(GREEN_OFF, GREEN_ON, numberOfBlinks); 
 	}
 	
+	/**
+	 * Constructor
+	 * @param blinkColor the blinking color
+	 * @param numberOfBlinks the number of blinks
+	 */
 	public LED (String blinkColor, int numberOfBlinks)
 	{
 		this(GREEN_OFF, blinkColor, numberOfBlinks); 
 	}
 	
+	/**
+	 * Constructor
+	 * @param restingColor the resting color
+	 * @param blinkColor the blink color
+	 * @param numberOfBlinks the number of blinks
+	 */
 	public LED (String restingColor, String blinkColor, int numberOfBlinks)
 	{
 		this.restingColor = new ImageIcon(restingColor); 
@@ -50,32 +76,56 @@ public class LED extends JLabel
 		this.numberOfBlinks = numberOfBlinks;  
 	}
 	
+	/**
+	 * Sets the resting color
+	 * @param restingColor the resting color
+	 */
 	public void setRestingColor(String restingColor)
 	{
 		this.restingColor = new ImageIcon(restingColor); 
 		setIcon(this.restingColor); 
 	}
 	
+	/**
+	 * Gets the resting color.
+	 * @return the resting color
+	 */
 	public ImageIcon getRestingColor()
 	{
 		return restingColor;
 	}
 	
+	/**
+	 * Set the number of blinks.
+	 * @param numberOfBlinks the number of blinks
+	 */
 	public void setNumberOfBlinks(int numberOfBlinks)
 	{
 		this.numberOfBlinks = numberOfBlinks;
 	}
 	
+	/**
+	 * Get the number of blinks.
+	 * @return the number of blinks
+	 */
 	public int getNumberOfBlinks()
 	{
 		return numberOfBlinks; 
 	}
 	
+	/**
+	 * Set the blinking color.
+	 * @param blinkColor the blinking color
+	 */
 	public void setBlinkColor(String blinkColor)
 	{
 		this.blinkColor = blinkColor;			
 	}
 	
+	/**
+	 * Gets the blinking color.
+	 * @return the blinking color
+	 */
 	public String getBlinkColor()
 	{
 		return blinkColor; 
