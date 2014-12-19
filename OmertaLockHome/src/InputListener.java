09c409de3e9d48f83ac524db98f7cad0c23c2481
gbrothers93@gmail.com <gbrothers93@gmail.com@c614bec4-09f8-51cd-9207-0c3074abb597>
2014-12-19 07:55:22 +0000

    Added comments to InputListener.java

--- OmertaLockHome/src/InputListener.java
+++ OmertaLockHome/src/InputListener.java
@@ -1,5 +1,9 @@
 import java.util.EventListener;
 
+/**
+ * Interface for the input and button listeners.
+ *
+ */
 public interface InputListener extends EventListener
 {
 	public void inputEventOccurred(InputEvent event);
